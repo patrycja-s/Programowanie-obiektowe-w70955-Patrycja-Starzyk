19bfc3442e2a5a8b838ad1a00d2c3bb7f30c7cf5
patrycja-s <patrycjastarzyk4@gmail.com>
2025-02-11 18:06:40 +0100

    Add files via upload

---
+++ laboratorium6/zad2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj nazwę pliku do wczytania: ");
+        string fileName = Console.ReadLine();  // Pobierz nazwę pliku od użytkownika
+
+        // Wczytanie zawartości pliku
+        try
+        {
+            string content = File.ReadAllText(fileName);  // Wczytaj zawartość pliku
+            Console.WriteLine("Zawartość pliku:");
+            Console.WriteLine(content);  // Wyświetl zawartość pliku
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Wystąpił błąd podczas wczytywania pliku: {ex.Message}");
+        }
+    }
+}
