a269af33de1253e0187e1c1822bdee97df4b8e3a
patrycja-s <patrycjastarzyk4@gmail.com>
2025-02-11 18:02:26 +0100

    Add files via upload

---
+++ laboratorium6/zad1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string albumNumber = "70956"; 
+        string fileName = "albumNumber.txt"; // nazwa pliku
+
+        // Zapisanie numeru albumu do pliku
+        try
+        {
+            File.WriteAllText(fileName, albumNumber);
+            Console.WriteLine($"Numer albumu zapisano do pliku {fileName}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Wystąpił błąd podczas zapisywania do pliku: {ex.Message}");
+        }
+    }
+}
+
